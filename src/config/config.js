27b0ff28f36cf5376fe3ad4d876e4b5569b50fa2
Arjan Aswal <arjanaswal09@gmail.com>
2022-05-18 12:31:41 +0530

    Connect sequelize

---
+++ src/config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  development: {
+    username: process.env.POSTGRES_USER ?? 'postgres',
+    password: process.env.POSTGRES_PASSWORD ?? 'postgres',
+    database: process.env.POSTGRES_DATABASE ?? 'postgres',
+    host: process.env.POSTGRES_HOST ?? 'localhost',
+    port: process.env.POSTGRES_PORT ?? 5432,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.POSTGRES_USER ?? 'postgres',
+    password: process.env.POSTGRES_PASSWORD ?? 'postgres',
+    database: process.env.POSTGRES_DATABASE ?? 'postgres',
+    host: process.env.POSTGRES_HOST ?? 'localhost',
+    port: process.env.POSTGRES_PORT ?? 5432,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.POSTGRES_USER ?? 'postgres',
+    password: process.env.POSTGRES_PASSWORD ?? 'postgres',
+    database: process.env.POSTGRES_DATABASE ?? 'postgres',
+    host: process.env.POSTGRES_HOST ?? 'localhost',
+    port: process.env.POSTGRES_PORT ?? 5432,
+    dialect: 'postgres',
+  },
+};
